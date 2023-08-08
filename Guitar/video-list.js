29381addf5ee6b1641ebb6899ae592d5fe4edfa3
mrsakofficial <139249904+mrsakofficial@users.noreply.github.com>
2023-08-08 07:00:13 +0700

    Add files via upload

---
+++ Guitar/video-list.js
@@ -0,0 +1,27 @@
+let allVideos = [
+   {
+      name: "Guitar Ep 1",
+      src: "https://www.youtube.com/embed/P5jxbIiGDb8",
+      id: "vid_1"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_7"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_7"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_7"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_7"
+   },
+]
---
+++ HipHop-Class/video-list.js
@@ -0,0 +1,22 @@
+let allVideos = [
+   {
+      name: "HipHop Class Ep 1",
+      src: "https://www.youtube.com/embed/xUXyvjfwApI",
+      id: "vid_1"
+   },
+   {
+      name: "HipHop Class Ep 2",
+      src: "https://www.youtube.com/embed/dtVBjL38JTg",
+      id: "vid_2"
+   },
+   {
+      name: "HipHop Class Ep 3",
+      src: "https://www.youtube.com/embed/fsLxkjdPhxE",
+      id: "vid_3"
+   },
+   {
+      name: "HipHop Class Ep 4",
+      src: "https://www.youtube.com/embed/eOTw9M2m26w",
+      id: "vid_4"
+   },
+]
