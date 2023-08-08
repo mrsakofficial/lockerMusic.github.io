8cbd44a6e4a51769d44d83639a914fd172deac4e
mrsakofficial <139249904+mrsakofficial@users.noreply.github.com>
2023-08-08 07:13:56 +0700

    Add files via upload

---
+++ Make-Orignal-Song/Level 1/video-list.js
@@ -0,0 +1,73 @@
+let allVideos = [
+   {
+      name: "Make Orignal Song Ep 1",
+      src: "https://www.youtube.com/embed/wq4rNA29CTE",
+      id: "vid_1"
+   },
+   {
+      name: "Make Orignal Song Ep 2",
+      src: "https://www.youtube.com/embed/VM0t5juju7I",
+      id: "vid_2"
+   },
+   {
+      name: "Make Orignal Song Ep 3",
+      src: "https://www.youtube.com/embed/bVBsUK-elc8",
+      id: "vid_3"
+   },
+   {
+      name: "Make Orignal Song Ep 4",
+      src: "https://www.youtube.com/embed/tuJZDV1O7xU",
+      id: "vid_4"
+   },
+   {
+      name: "Make Orignal Song Ep 5",
+      src: "https://www.youtube.com/embed/e1iqZYQhSW8",
+      id: "vid_5"
+   },
+   {
+      name: "Make Orignal Song Ep 6",
+      src: "https://www.youtube.com/embed/6ojGAr0_7rw",
+      id: "vid_6"
+   },
+   {
+      name: "Make Orignal Song Ep 7",
+      src: "https://www.youtube.com/embed/CZ4Qmp7Pdf8",
+      id: "vid_7"
+   },
+   {
+      name: "Make Orignal Song Ep 8",
+      src: "https://www.youtube.com/embed/db4Ty8EtK-Q",
+      id: "vid_8"
+   },
+   {
+      name: "Make Orignal Song Ep 9",
+      src: "https://www.youtube.com/embed/wCs7I_QVicY",
+      id: "vid_9"
+   },
+   {
+      name: "Make Orignal Song Ep 10",
+      src: "https://www.youtube.com/embed/8B2ThjnzULw",
+      id: "vid_10"
+   },
+   {
+      name: "រMake Orignal Song Ep 11",
+      src: "https://www.youtube.com/embed/KZpfBlbIfhM",
+      id: "vid_11"
+   },
+   {
+      name: "Make Orignal Song Ep 12",
+      src: "https://www.youtube.com/embed/KoFG-_wzLH0",
+      id: "vid_12"
+   },
+   {
+      name: "Make Orignal Song Ep 13",
+      src: "https://www.youtube.com/embed/_pAeRRtWREE",
+      id: "vid_13"
+   },
+   {
+      name: "Make Orignal Song Ep 14 End",
+      src: "https://www.youtube.com/embed/csiOs4FIQ0M",
+      id: "vid_14"
+   },
+   
+]
---
+++ Make-Orignal-Song/Level 2/video-list.js
@@ -0,0 +1,79 @@
+let allVideos = [
+
+   {
+      name: "Make Orignal Song Ep 1",
+      src: "https://www.youtube.com/embed/SFIjAqzYau4",
+      id: "vid_1"
+   },
+   {
+      name: "Make Orignal Song Ep 2",
+      src: "https://www.youtube.com/embed/vrc9N-z4BQc",
+      id: "vid_2"
+   },
+   {
+      name: "Make Orignal Song Ep 3",
+      src: "https://www.youtube.com/embed/nTaTGeoyZY0",
+      id: "vid_3"
+   },
+   {
+      name: "Make Orignal Song Ep 4",
+      src: "https://www.youtube.com/embed/5S6l6e-l7Nw",
+      id: "vid_4"
+   },
+   {
+      name: "Make Orignal Song Ep 5",
+      src: "https://www.youtube.com/embed/efcihD4mVT8",
+      id: "vid_5"
+   },
+   {
+      name: "Make Orignal Song Ep 6",
+      src: "https://www.youtube.com/embed/kaWIEDL4r1g",
+      id: "vid_6"
+   },
+   {
+      name: "Make Orignal Song Ep 7",
+      src: "https://www.youtube.com/embed/gVjN13plAOA",
+      id: "vid_7"
+   },
+   {
+      name: "Make Orignal Song Ep 8",
+      src: "https://www.youtube.com/embed/W2oulRmCirQ",
+      id: "vid_8"
+   },
+   {
+      name: "Make Orignal Song Ep 9",
+      src: "https://www.youtube.com/embed/WI0dRQGWVbc",
+      id: "vid_9"
+   },
+   {
+      name: "Make Orignal Song Ep 10",
+      src: "https://www.youtube.com/embed/FGLLFYvkdd0",
+      id: "vid_10"
+   },
+   {
+      name: "រMake Orignal Song Ep 11",
+      src: "https://www.youtube.com/embed/-SXhnqjNpV8",
+      id: "vid_11"
+   },
+   {
+      name: "Make Orignal Song Ep 12",
+      src: "https://www.youtube.com/embed/Vlfa3v8AW5s",
+      id: "vid_12"
+   },
+   {
+      name: "Make Orignal Song Ep 13",
+      src: "https://www.youtube.com/embed/gcDYjKqLupA",
+      id: "vid_13"
+   },
+   {
+      name: "Make Orignal Song Ep 14",
+      src: "https://www.youtube.com/embed/AxINh5qFUno",
+      id: "vid_14"
+   },
+   {
+      name: "Make Orignal Song Ep 15 End",
+      src: "https://www.youtube.com/embed/GcnEAtBB2QY",
+      id: "vid_14"
+   },
+   
+]
