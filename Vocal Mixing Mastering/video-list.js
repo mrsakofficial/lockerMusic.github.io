ff2f3fd8325a5fc28be8e7b0267ec0a4e10f1535
mrsakofficial <139249904+mrsakofficial@users.noreply.github.com>
2023-08-08 07:03:09 +0700

    Add files via upload

---
+++ Vocal Mixing Mastering/video-list.js
@@ -0,0 +1,102 @@
+let allVideos = [
+   {
+      name: "​Vocal Mixing Mastering Ep 1",
+      src: "https://www.youtube.com/embed/j-t3R3Q-nA4",
+      id: "vid_1"
+   },
+   {
+      name: "​Vocal Mixing Mastering Ep 2",
+      src: "https://www.youtube.com/embed/z-pdmD5XfTw",
+      id: "vid_2"
+   },
+   {
+      name: "​Vocal Mixing Mastering Ep 3",
+      src: "https://www.youtube.com/embed/WZ3mqT2k68M",
+      id: "vid_3"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_4"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_5"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_6"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_7"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_8"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_9"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_10"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_11"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_12"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_13"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_14"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_15"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_16"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_17"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_18"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_19"
+   },
+   {
+      name: "រង់ចាំមេរៀនបន្ត",
+      src: "https://www.youtube.com/embed/uyHXCTy-be8",
+      id: "vid_20"
+   }
+]
