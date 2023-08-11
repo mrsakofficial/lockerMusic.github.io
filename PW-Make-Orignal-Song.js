04f6b9402030885918869f14fd1e793fd1c112cd
mrsakofficial <139249904+mrsakofficial@users.noreply.github.com>
2023-08-12 00:51:56 +0700

    Update PW-Make-Orignal-Song.js

--- PW-Make-Orignal-Song.js
+++ PW-Make-Orignal-Song.js
@@ -14,6 +14,10 @@ function auth(){
     {
       window.location.href=("Make-Orignal-Song/Make-Orignal-Song.html")
     }
+    if(username=="admin" && password=="1234")
+    {
+      window.location.href=("Vocal Mixing Mastering/​Vocal Mixing Mastering.html")
+    }
 
     else
     {
